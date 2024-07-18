5742def1ce15cc0684cd50f9ec794bb73d7bfbe5
lachlan <lachlan.00@gmail.com>
2024-07-18 20:36:33 +1000

    rename the files corectly

--- 6.5.0/default/data/bin/run.tpl_apache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copy Ampache config .dist files
-cp -p /var/tmp/*.dist /var/www/config/
-
-# Start Supervisor to manage all the processes
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
